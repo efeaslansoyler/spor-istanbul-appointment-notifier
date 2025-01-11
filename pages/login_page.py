1d459d9a6c86351242d27bd01c7ec0d58e4d715c
efeaslansoyler <efeaslan1703@gmail.com>
2025-01-11 16:13:46 +0300

    added a login page automation

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+from utils.logger import log_with_timestamp
+
+def login(driver, username, password) -> None:
+    try:
+        log_with_timestamp("Navigating to login page")
+        driver.get("https://online.spor.istanbul/uyegiris")
+        
+        log_with_timestamp("Login page reached successfully")
+        username_field = driver.find_element(By.ID, "txtTCPasaport")
+        password_field = driver.find_element(By.ID, "txtSifre")
+        login_button = driver.find_element(By.ID, "btnGirisYap")
+
+        log_with_timestamp("Filling in username and password")
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        login_button.click()
+        if "anasayfa" not in driver.current_url:
+            log_with_timestamp("Login failed")
+            raise Exception("Login failed")
+        log_with_timestamp("Login successful")
+    except Exception as e:
+        log_with_timestamp(f"Failed to navigate to login page: {str(e)}")
+        raise e
