ad0b51b02917398c2efd0f2d678de834741552fb
efeaslansoyler <efeaslan1703@gmail.com>
2025-01-13 20:38:53 +0300

    enchance session availability message for make appointment feature

--- utils/check_available_session.py
+++ utils/check_available_session.py
@@ -15,17 +15,21 @@ def check_available_session(driver) -> str:
     log_with_timestamp("Checking for available sessions")
     try:
         available_sessions = driver.find_elements(By.CLASS_NAME, "well")
+        message = ""
+        i = 1
         if not available_sessions:
             log_with_timestamp("No available sessions found")
-            return "Uygun seans bulunamadı"
+            message += "Uygun seans bulunamadı"
+            return message
         log_with_timestamp(f"Found {len(available_sessions)} available sessions")
-        message = ""
         for session in available_sessions:
             session_day = session.find_element(By.XPATH, "./ancestor::div[contains(@class, 'panel')]/div[contains(@class, 'panel-heading')]/h3[contains(@class, 'panel-title')]")
             session_time = session.find_element(By.XPATH, ".//span[2]")
             session_quota = session.find_element(By.XPATH, ".//span[3]")
 
-            message += f"\n Seans günü: {extract_text_via_js(session_day)}, Seans Zamanı: {extract_text_via_js(session_time)}, Seans Kapasitesi: {extract_text_via_js(session_quota)}\n"
+            message += f"\n {[i]} - Seans günü: {extract_text_via_js(session_day)}, Seans Zamanı: {extract_text_via_js(session_time)}, Seans Kapasitesi: {extract_text_via_js(session_quota)}\n"
+            i += 1
+        message += f"\n Randevu almak istiyorsanız lütfen istediğiniz seansın numarasını giriniz. Birden fazla seans almak için numaraları virgülle ayırabilirsiniz. Örnek: 1,2,3"
         return message
     except Exception as e:
         log_with_timestamp(f"Error checking for available sessions: {str(e)}")
